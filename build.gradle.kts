38702c7a47edfac5070c2b79609c845b2447fb4b
AnswerForMystery <jim20071128@163.com>
2022-11-25 21:05:30 +0800

    Merge

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,12 +48,12 @@ tasks.shadowJar{
 }
 
 tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 publishing {
