387979cb42f9c476cbb8f0f4ff155156941b5f57
killerprojecte <admin@fastmcmirror.org>
2023-06-27 17:15:01 +0800

    Update poke types from mirai

--- src/main/kotlin/com/xbaimiao/mirai/message/component/impl/PokeType.kt
+++ src/main/kotlin/com/xbaimiao/mirai/message/component/impl/PokeType.kt
@@ -1,7 +1,29 @@
 package com.xbaimiao.mirai.message.component.impl
 
 enum class PokeType(poke: String) {
-    Poke("Poke"), ShowLove("ShowLove"), Like("Like"), Heartbroken("Heartbroken"), SixSixSix("SixSixSix"), FangDaZhao("FangDaZhao");
+    Poke("戳一戳"),
+    ShowLove("比心"),
+    Like("点赞"),
+    Heartbroken("心碎"),
+    SixSixSix("666"),
+    FangDaZhao("放大招"),
+
+    // Update from mirai-core-api
+    ChuoYiChuo("戳一戳"),
+    BiXin("比心"),
+    DianZan("点赞"),
+    XinSui("心碎"),
+    LiuLiuLiu("666"),
+    BaoBeiQiu("宝贝球"),
+    Rose("玫瑰花"),
+    ZhaoHuanShu("召唤术"),
+    RangNiPi("让你皮"),
+    JieYin("结印"),
+    ShouLei("手雷"),
+    GouYin("勾引"),
+    ZhuaYiXia("抓一下"),
+    SuiPing("碎屏"),
+    QiaoMen("敲门");
 
     companion object {
         fun typeToString(type: PokeType): String {
